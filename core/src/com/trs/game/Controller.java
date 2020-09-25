0ca67870d6357e55eb7a0b5938f16328cd1da3e4
GammelJAN <jan.ehehalt@gmail.com>
2020-09-25 20:17:47 +0200

    test

--- core/src/com/trs/game/Controller.java
+++ core/src/com/trs/game/Controller.java
@@ -2,18 +2,20 @@ package com.trs.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 public class Controller extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	ShapeRenderer renderer;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		renderer = new ShapeRenderer();
 	}
 
 	@Override
@@ -21,13 +23,18 @@ public class Controller extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
 		batch.end();
+		renderer.begin(ShapeRenderer.ShapeType.Filled);
+		renderer.setColor(Color.WHITE);
+		renderer.rect(50,50,0,0,50,50,1,1,45);
+		renderer.setColor(Color.BLACK);
+		renderer.circle(50,50,5);
+		renderer.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		renderer.dispose();
 	}
 }
--- core/src/com/trs/game/view/View.java
+++ core/src/com/trs/game/view/View.java
@@ -1,4 +1,13 @@
 package com.trs.game.view;
 
+import com.badlogic.gdx.math.Polygon;
+import com.badlogic.gdx.math.Rectangle;
+
 public class View {
+    Rectangle r;
+    Polygon p;
+
+    View(){
+
+    }
 }
